da6b44ae50e25db7c406f6b1669be053cab7706f
brunosg1 <bruno.silva_gomes@hotmail.com>
2023-08-28 20:22:56 -0300

    Modelos

--- lojaDeRoupas/lojaDeRoupas/Program.cs
+++ lojaDeRoupas/lojaDeRoupas/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace lojaDeRoupas
+﻿using lojaDeRoupas.Modelos;
+
+
+namespace lojaDeRoupas
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Cliente cliente = new Cliente
+            {
+                Nome = "Bruno",
+                Endereco = "Rua riachuelo",
+                Sobrenome = "Gomes",
+                Numerotelefone = "69999181330"
+            };
+
+            Console.WriteLine("Cliente " + cliente.Nome + " " + cliente.Sobrenome + " Cadastrado");
         }
     }
 }
